8e45559f186d5ad2da6ca551fdea0468ba80a5aa
ThunderStormOwl <lausrock@gmail.com>
2022-09-12 16:45:12 -0300

    Native set up

---
+++ mobile/App.tsx
@@ -0,0 +1,37 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sup dog!</Text>
+      <Button title={"test button"} />
+      <StatusBar style="auto" backgroundColor="red" />
+    </View>
+  );
+}
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#000",
+    fontSize: 44,
+  },
+});
